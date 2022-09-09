fa4cad3b12bd610ea65961769e25aadc0aa07cf0
kumavale <kumavale.hyperion@gmail.com>
2022-09-10 07:36:20 +0900

    画像ファイル削除機能を追加

--- src/list.rs
+++ src/list.rs
@@ -56,6 +56,10 @@ pub async fn list(pool: web::Data<PgPool>) -> Result<HttpResponse, Error> {
 }
 
 pub async fn list_modify(pool: web::Data<PgPool>, params: web::Form<ListModifyParams>) -> Result<HttpResponse, Error> {
+    #[derive(sqlx::FromRow)]
+    struct Image {
+        pub filename: String,
+    }
     let name   = &params.name;
     let submit = &params.submit;
     let (initial, next) = util::split_pair(name).unwrap();
@@ -65,6 +69,24 @@ pub async fn list_modify(pool: web::Data<PgPool>, params: web::Form<ListModifyPa
             todo!()
         }
         "Delete" => {
+            // 画像ファイル削除
+            let image = sqlx::query_as::<_, Image>("
+                SELECT
+                    list.image AS filename
+                FROM
+                    list
+                WHERE
+                    initial=$1 AND next=$2
+                ")
+                .bind(&initial)
+                .bind(&next)
+                .fetch_one(&**pool)
+                .await
+                .unwrap();
+            let filepath = format!("img/{}", image.filename);
+            std::fs::remove_file(filepath).unwrap();
+
+            // DBレコード削除
             sqlx::query(r#"
                 DELETE FROM
                     list
