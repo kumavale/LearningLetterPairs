102f8df9db9aa1329a0643cefe5ecddcee8b70b7
kumavale <kumavale.hyperion@gmail.com>
2022-10-07 13:40:26 +0900

    前/次の問題を表示できるようにした

--- script/quiz.js
+++ script/quiz.js
@@ -1,3 +1,8 @@
+// レターペアの循環リスト
+// TODO: リストにする
+var LP_LISTS;
+var LP_POS = 0;  // 現在LP_LISTSの何番目を表示しているか
+
 window.onload = function() {
     // シャッフルされたレターペアのリストを取得
     fetch('/shuffle_lp', { method: 'POST' })
@@ -5,26 +10,10 @@ window.onload = function() {
             return response.json();
         })
         .then((jsonobj) => {
-            var lp_name_element    = document.querySelector('#lp_name');
-            var lp_objects_element = document.querySelector('#lp_objects');
-            var lp_image_element   = document.querySelector('#lp_image');
-
-            const lists = jsonobj.lists;
-
-            // レターペアのタイトルを設定
-            lp_name_element.innerText = lists[0].name;
-
-            // レターペアの内容を設定
-            lists[0].objects.forEach(object => {
-                var li = document.createElement('li');
-                li.innerText = object;
-                lp_objects_element.appendChild(li);
-            });
-
-            // 画像ファイルの設定
-            var img = document.createElement('img');
-            img.src = "static/img/" + lists[0].image;
-            lp_image_element.appendChild(img);
+            // グローバル変数に格納
+            LP_LISTS = jsonobj.lists;
+            // 問題を表示
+            display_question();
         })
         .catch((e) => {
             alert("Failed to get contents");
@@ -32,13 +21,68 @@ window.onload = function() {
 
     // [Answer]ボタン押下時のイベントハンドラ設定
     var control_answer = document.querySelector('#control_answer');
-    control_answer.addEventListener('click', show_answer);
+    control_answer.addEventListener('click', () => { show_answer('visible'); });
+
+    // [Next]ボタン押下時のイベントハンドラ設定
+    var control_next = document.querySelector('#control_next');
+    control_next.addEventListener('click', next_lp);
+
+    // [Prev]ボタン押下時のイベントハンドラ設定
+    var control_prev = document.querySelector('#control_prev');
+    control_prev.addEventListener('click', prev_lp);
 };
 
-// visibility:hiddenになっている要素を可視化する
-function show_answer() {
+// 問題を表示
+function display_question() {
+    var lp_name_element    = document.querySelector('#lp_name');
     var lp_objects_element = document.querySelector('#lp_objects');
     var lp_image_element   = document.querySelector('#lp_image');
-    lp_objects_element.style.visibility = 'visible';
-    lp_image_element  .style.visibility = 'visible';
+
+    // 解答を非表示に設定
+    show_answer('hidden');
+
+    // レターペアのタイトルを設定
+    lp_name_element.innerText = LP_LISTS[LP_POS].name;
+
+    // レターペアの内容を設定
+    lp_objects_element.innerHTML = '';
+    LP_LISTS[LP_POS].objects.forEach(object => {
+        var li = document.createElement('li');
+        li.innerText = object;
+        lp_objects_element.appendChild(li);
+    });
+
+    // 画像ファイルの設定
+    lp_image_element.innerHTML = '';
+    var img = document.createElement('img');
+    img.src = "static/img/" + LP_LISTS[LP_POS].image;
+    lp_image_element.appendChild(img);
+}
+
+// visibility:hiddenになっている要素を可視化する
+function show_answer(visibility) {
+    var lp_objects_element = document.querySelector('#lp_objects');
+    var lp_image_element   = document.querySelector('#lp_image');
+    lp_objects_element.style.visibility = visibility;
+    lp_image_element  .style.visibility = visibility;
+}
+
+// 次の問題を表示
+function next_lp() {
+    if (LP_POS < LP_LISTS.length - 1) {
+        LP_POS += 1;
+    } else {
+        LP_POS = 0;
+    }
+    display_question();
+}
+
+// 前の問題を表示
+function prev_lp() {
+    if (LP_POS > 0) {
+        LP_POS -= 1;
+    } else {
+        LP_POS = LP_LISTS.length - 1;
+    }
+    display_question();
 }
