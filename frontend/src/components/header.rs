b195e63b63d2103e5ba95f07ebb2a4e6f2d2c427
kumavale <kumavale.hyperion@gmail.com>
2023-06-19 22:15:28 +0900

    fix: `header`コンポーネントのtypoと、ログインボタンの右寄せを修正

--- frontend/src/components/header.rs
+++ frontend/src/components/header.rs
@@ -3,13 +3,13 @@ use yew::prelude::*;
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
-        <heaer>
+        <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand m-1 ms-4 me-4 h1" href="/">{"Learning Letter Pairs"}</a>
-                <div class="justify-content-end">
+                <div class="ms-auto">
                     <a class="btn btn-outline-light me-2" href="/login">{"Login"}</a>
                 </div>
             </nav>
-        </heaer>
+        </header>
     }
 }
